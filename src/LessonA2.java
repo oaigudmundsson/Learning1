53e6d33a5a6887b99a210055b349297ac63e8775
Oai <oai.gudmundsson@ownit.se>
2020-01-16 23:18:16 +0100

    Add lessonA2

---
+++ src/LessonA2.java
@@ -0,0 +1,24 @@
+import com.matte.Calculator;
+import com.matte.Ai;
+
+public class LessonA2 {
+
+    // complete this function
+    public String up1()
+    {
+
+    }
+
+    public static void main(String args[]) {
+        LessonA2 lesson = new LessonA2();
+
+        // up1. Create a function with 1 string param. Name it to `yourName`
+        // return string "Hello {yourName} there".
+        // Then print that variable here
+        String a = lesson.up1();
+        System.out.println(a);
+
+    }
+
+
+}
