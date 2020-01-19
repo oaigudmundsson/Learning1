b36b041dcdbe2356bd1c01fbe189b89b73a99f0c
Oai <oai.gudmundsson@ownit.se>
2020-01-19 22:27:13 +0100

    Add lessonA2

--- src/LessonA2.java
+++ src/LessonA2.java
@@ -4,9 +4,10 @@ import com.matte.Ai;
 public class LessonA2 {
 
     // complete this function
-    public String up1()
+    public String up1(String yourName)
     {
-
+        String hello = "Hello "+ yourName + " there";
+        return hello;
     }
 
     public static void main(String args[]) {
@@ -15,10 +16,13 @@ public class LessonA2 {
         // up1. Create a function with 1 string param. Name it to `yourName`
         // return string "Hello {yourName} there".
         // Then print that variable here
-        String a = lesson.up1();
+        String a = lesson.up1("Oai");
         System.out.println(a);
 
-    }
 
+        // up2. Create a function return bool. Required 1 param as int
+        // Return True if param is bigger than 10, else False
+
+    }
 
 }
